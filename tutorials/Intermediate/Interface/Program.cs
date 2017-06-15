7a54588e52204f920e716547f887da4dd11e27f7
Marco Pineda <mapineda1222@gmail.com>
2017-06-15 12:54:36 -0500

    updated interace extensibility tutorial

--- tutorials/Intermediate/Interface/Program.cs
+++ tutorials/Intermediate/Interface/Program.cs
@@ -1,12 +1,14 @@
 using System;
+using System.IO;
 
 namespace Extensibility {
+
 
   class Program
   {
     static void Main(string[] args)
     {
-        var dbMigrator = new DbMigrator(new ConsoleLogger());
+        var dbMigrator = new DbMigrator(new FileLogger("E:\\desktop\\log.txt"));
         dbMigrator.Migrate();
     }
   }
